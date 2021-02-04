f2112fcacf71e34c88e478caa45382d372f31151
jos3duardo <jos3duardolopes@gmail.com>
2021-02-03 22:09:23 -0400

    formulario de transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(title: Text('Transferências'),),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      )
-    )
-  );
+  runApp(BankApp());
+}
+
+class BankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: FormularioTransferencia(),
+        )
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferência'),),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 16.0
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '000000'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                    fontSize: 16.0
+                ),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on_outlined),
+                    labelText: 'Valor',
+                    hintText: '0.00'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Salvar'),
+            )
+          ],
+        ));
+  }
 }
 
 class ListaTransferencias extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencias(Transferencia(100.5, 10000)),
-        ItemTransferencias(Transferencia(230.4, 20000)),
-        ItemTransferencias(Transferencia(500.0, 50000)),
-        ItemTransferencias(Transferencia(150.0, 13000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: Text('Transferências'),),
+      body: Column(
+        children: [
+          ItemTransferencias(Transferencia(100.5, 10000)),
+          ItemTransferencias(Transferencia(230.4, 20000)),
+          ItemTransferencias(Transferencia(500.0, 50000)),
+          ItemTransferencias(Transferencia(150.0, 13000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }

cd3488a6befb1b917c9a5cc816fae7cdc20ebd3c
jos3duardo <jos3duardolopes@gmail.com>
2021-02-03 21:46:54 -0400

    refatorando listagem de transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('10000'),
-              )
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('10000'),
-              )
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
@@ -29,4 +12,44 @@ void main() {
       )
     )
   );
+}
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencias(Transferencia(100.5, 10000)),
+        ItemTransferencias(Transferencia(230.4, 20000)),
+        ItemTransferencias(Transferencia(500.0, 50000)),
+        ItemTransferencias(Transferencia(150.0, 13000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencias extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+  ItemTransferencias(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        )
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
 }
